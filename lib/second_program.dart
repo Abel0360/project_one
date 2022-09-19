5698442f6f9818038deb831f0f1f224424cc20aa
unknown <ajithkumarklm1998@gmail.com>
2022-09-19 11:11:59 +0530

    Initial commit

---
+++ lib/second_program.dart
@@ -0,0 +1,8 @@
+void main(){
+  String name ="abel";
+  int age = 22;
+  print("my name is ${name}");
+  print("i am ${age} yrs old");
+
+
+}
---
+++ lib/third_program.dart
@@ -0,0 +1,4 @@
+void main(){
+  int a=1 , b=2;
+      print("sum = ${a+b}");
+}
